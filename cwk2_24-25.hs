2e5987b32c3ca72add8cfcc243a7f870591926fd
Bobby Ta <49725144+bobby-ta@users.noreply.github.com>
2025-01-10 11:46:15 +0100

    Solve coursework question 2

--- cwk2_24-25.hs
+++ cwk2_24-25.hs
@@ -24,3 +24,15 @@ rotate180 xs = transpose (transpose xs)
 
 rotate270 :: Horse -> Horse
 rotate270 xs = reverse (mirror (transpose xs))
+
+--Question 2
+tribonacci :: Int -> [Int]
+--tribonacci n = map tribonacci' [0..n] where
+    --tribonacci' 0 = 0
+    --tribonacci' 1 = 0
+    --tribonacci' 2 = 1
+    --tribonacci' n = tribonacci' (n - 1) + tribonacci' (n - 2) + tribonacci' (n - 3)
+tribonacci n = take (n) (map head (iterate (\[a, b, c] -> [b, c, a + b + c]) [0, 0, 1]))
+
+lazycaterer :: Int -> [Int]
+lazycaterer n = take (n) (map head (iterate (\[a, b] -> [b, b + (b - a + 1)]) [1, 2]))
